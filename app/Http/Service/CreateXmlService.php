95cc729039d680d933416babfad0bc91e944c89c
MaverickJames21 <maverick.j.hegi@gmail.com>
2021-12-09 12:28:23 +0100

    commit

--- app/Http/Service/CreateXmlService.php
+++ app/Http/Service/CreateXmlService.php
@@ -41,13 +41,14 @@ class CreateXmlService {
 // -------
 
 
-        // Create payload
+        // Create invoice:payload
         $invoice_payload = $xmlDoc->createElement('invoice:payload');
-        $invoice_invoice = $xmlDoc->createElement('invoice:invoice');
         // Set attribute node invoice:payload
         $invoice_payload->setAttributeNode(new DOMAttr('type', 'invoice'));
         $invoice_payload->setAttributeNode(new DOMAttr('copy', 0));
         $invoice_payload->setAttributeNode(new DOMAttr('storno', 0));
+        // Create invoice:invoice
+        $invoice_invoice = $xmlDoc->createElement('invoice:invoice');
         // Set attribute node invoice:invoice
         $invoice_invoice->setAttributeNode(new DOMAttr('request_timestamp', 1619688895));
         $invoice_invoice->setAttributeNode(new DOMAttr('request_date', '2021-04-25T00:00:00'));
@@ -55,8 +56,6 @@ class CreateXmlService {
         // save child invoice:invoice which is in invoice:playload
         $invoice_payload->appendChild($invoice_invoice);
 
-        // Save payload.
-        $root->appendChild($invoice_payload);
 
 
 // ------
@@ -66,31 +65,26 @@ class CreateXmlService {
         // Set attribute node invoice:invoice
         $invoice_body->setAttributeNode(new DOMAttr('role', 'hospital'));
         $invoice_body->setAttributeNode(new DOMAttr('place', 'hospital'));
-        // save child invoice:invoice which is in invoice:playload
-        $invoice_payload->appendChild($invoice_body);
-        // Save payload.
-        $root->appendChild($invoice_payload);
 
-// ------
+// -----
+
+        /**
+         * Create invoice prolog.
+         */
 
         // Create invoice:prolog which is in invoice:payload
         $invoice_prolog = $xmlDoc->createElement('invoice:prolog');
         $invoice_package = $xmlDoc->createElement('invoice:package');
+
         // Set attribute node invoice:package
         $invoice_package->setAttributeNode(new DOMAttr('name', 'GeneralInvoiceRequestTest'));
         $invoice_package->setAttributeNode(new DOMAttr('copyright', 'suva 2000-21'));
         $invoice_package->setAttributeNode(new DOMAttr('version', '100021'));
-        // save child invoice:package which is in invoice:prolog
-        $invoice_prolog->appendChild($invoice_package);
-        $invoice_payload->appendChild($invoice_prolog);
-        // Save payload.
-        $root->appendChild($invoice_payload);
-
-// -----
 
         // Create invoice:depends_on which is in invoice:generator
         $invoice_generator = $xmlDoc->createElement('invoice:generator');
         $invoice_depends_on = $xmlDoc->createElement('invoice:depends_on');
+
         // Set attribute node invoice:generator and invoice:depends_on
         $invoice_generator->setAttributeNode(new domAttr('name','GeneralInvoiceRequestManager 4.50.019'));
         $invoice_generator->setAttributeNode(new domAttr('copyright','suva 2000-21'));
@@ -99,60 +93,154 @@ class CreateXmlService {
         $invoice_depends_on->setAttributeNode(new domAttr('copyright','Suva'));
         $invoice_depends_on->setAttributeNode(new domAttr('version',100));
         $invoice_depends_on->setAttributeNode(new domAttr('id',1007090101));
+
         // save child invoice:depends_on which is in invoice:generator
         $invoice_generator->appendChild($invoice_depends_on);
         $invoice_prolog->appendChild($invoice_generator);
 
-// -----
+        // save child invoice:package which is in invoice:prolog
+        $invoice_prolog->appendChild($invoice_package);
+        $invoice_body->appendChild($invoice_prolog);
 
+
+        /**
+         * Create invoice invoice:remark.
+         */
 
         // Create invoice:remark with texte Between tag/node(balise) wich is in invoice:payload
         $texte_remark= 'Lorem ipsum per nostra mi fune torectum mikonstra.diloru si limus mer fin per od per nostra mi fune torectum mi konstradiloru si limus mer fin itorectum mi konstradiloruko.';
-        $invoice_remark = $xmlDoc->createElement('invoice:remark',$texte_remark);
+        $invoice_remark = $xmlDoc->createElement('invoice:remark', $texte_remark);
         // save child invoice:remark which is in invoice:payload
-        $invoice_payload->appendChild($invoice_remark);
+        $invoice_body->appendChild($invoice_remark);
 
 
-
-// -----
+        /**
+         * Create invoice invoice:invoice_tiers_payant.
+         */
 
         // Create invoice:biller which is in invoice:tiers_payant
         $invoice_tiers_payant = $xmlDoc->createElement('invoice:invoice_tiers_payant');
         $invoice_biller = $xmlDoc->createElement('invoice:biller');
+
         // Set attribute node invoice:biller which is in invoice:tiers_payant
         $invoice_biller->setAttributeNode(new domAttr('ean_party',2011234567890));
         $invoice_biller->setAttributeNode(new domAttr('zsr','H121111'));
         $invoice_biller->setAttributeNode(new domAttr('uid_number','CHE108791452'));
+
+        // Create invoice:company which is in invoice:biller
+        $invoice_company = $xmlDoc->createElement('invoice:company');
+        $invoice_company_name = $xmlDoc->createElement('invoice:companyname', 'Biller AG');
+        $invoice_company->appendChild($invoice_company_name);
+        $invoice_department = $xmlDoc->createElement('invoice:department', 'Abteilung Inkasso');
+        $invoice_company->appendChild($invoice_department);
+        $invoice_postal = $xmlDoc->createElement('invoice:postal');
+        $invoice_street = $xmlDoc->createElement('invoice:street', 'Billerweg 128');
+        $invoice_zip = $xmlDoc->createElement('invoice:zip', 4414);
+        $invoice_city = $xmlDoc->createElement('invoice:city', 'Frenkendorf');
+        $invoice_postal->appendChild($invoice_street);
+        $invoice_postal->appendChild($invoice_zip);
+        $invoice_postal->appendChild($invoice_city);
+        $invoice_company->appendChild($invoice_postal);
+        $invoice_biller->appendChild($invoice_company);
+
+
+
         // save child invoice:depends_on which is in invoice:generator
         $invoice_tiers_payant->appendChild($invoice_biller);
-        $invoice_payload->appendChild($invoice_tiers_payant);
+        $invoice_body->appendChild($invoice_tiers_payant);
 
 // -----
 
-        // Create invoice:company which is in invoice:biller
-        $texte_company_name = 'Biller AG';
-        $invoice_company_name = $xmlDoc->createElement('invoice:companyname',$texte_company_name);
-
-        $invoice_company = $xmlDoc->createElement('invoice:company');
+/*
         $invoice_company_name = $xmlDoc->createElement('invoice:companyname');
         // $invoice_department = $xmlDoc->createElement('invoice:department');
         // Set attribute node invoice:company/companyname/departement which is in invoice:biller
-        $invoice_company->appendChild($invoice_company_name);
         $invoice_biller->appendChild($invoice_company);
         $invoice_payload->appendChild($invoice_biller);
 
 
+// -----
+
+
+        // Create invoice:debitore which is in invoice:biller
+
+        $invoice_debitor = $xmlDoc->createElement('invoice:debitor');
+
+        $invoice_debitor->setAttributeNode(new domAttr('ean_party',7634567890000));
+
+
+
+        $invoice_payload->appendChild($invoice_debitor);
+
+
+
+// -----
+
+
+         // Create invoice:provider which is in invoice:payload
+
+        $invoice_provider = $xmlDoc->createElement('invoice:provider');
+
+        $invoice_provider->setAttributeNode(new domAttr('ean_party',7634567890111));
+        $invoice_provider->setAttributeNode(new domAttr('zsr','P123456'));
 
 
 
 
-        //  // Create invoice:remark with texte Between tag/node/(balise) wich is in invoice:payload
+        $invoice_payload->appendChild($invoice_provider);
 
-        //  $texte_remark= 'Lorem ipsum per nostra mi fune torectum mikonstra.diloru si limus mer fin per od per nostra mi fune torectum mi konstradiloru si limus mer fin itorectum mi konstradiloruko.';
-        //  $invoice_remark = $xmlDoc->createElement('invoice:remark',$texte_remark);
+// -----
 
-        //  // save child invoice:remark which is in invoice:payload
-        //  $invoice_payload->appendChild($invoice_remark);
+
+         // Create invoice:provider which is in invoice:payload
+
+         $invoice_insurance = $xmlDoc->createElement('invoice:insurance');
+
+         $invoice_insurance->setAttributeNode(new domAttr('ean_party',7634567890000));
+
+
+
+
+         $invoice_payload->appendChild($invoice_insurance);
+*/
+// -----
+
+        /*
+
+        // Create invoice:biller which is in invoice:tiers_payant
+        $invoice_service = $xmlDoc->createElement('invoice:invoice_service');
+
+        // Set attribute node invoice:biller which is in invoice:tiers_payant
+        $invoice_service->setAttributeNode(new domAttr('record_id',1));
+        $invoice_service->setAttributeNode(new domAttr('tariff_type',020));
+        $invoice_service->setAttributeNode(new domAttr('code','TR11A'));
+        $invoice_service->setAttributeNode(new domAttr('session',1));
+        $invoice_service->setAttributeNode(new domAttr('quantity',1));
+        $invoice_service->setAttributeNode(new domAttr('date_begin','2021-02-10T00:00:00'));
+
+        $invoice_service->setAttributeNode(new domAttr('ean_party',2011234567890));
+        $invoice_service->setAttributeNode(new domAttr('ean_party',2011234567890));
+
+        // save child invoice:depends_on which is in invoice:generator
+        $invoice_body->appendChild($invoice_service);
+        */
+
+        // Save save.
+        $invoice_payload->appendChild($invoice_body);
+
+        // Save payload.
+        $root->appendChild($invoice_payload);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
